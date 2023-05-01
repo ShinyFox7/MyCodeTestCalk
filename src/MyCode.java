dc249052b20719e2318c6bc518db9522eaff8a0b
ShinyFox7 <arkona.2188@gmail.com>
2023-05-01 05:37:58 -0700

    Initial commit

---
+++ src/MyCode.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MyCode {
+        public static void main(String[] args){
+            Scanner sr = new Scanner(System.in);
+            System.out.println("первое число");
+            int c1 = sr.nextInt();
+            System.out.println("второе число");
+            int c2 = sr.nextInt();
+            System.out.println("что с ними делаем?");
+
+        }
+    }
